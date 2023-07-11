de52f71d4d9051351059b37a9b4cc2c420e2b333
Parker Bedlan <pbedlan@nepris.com>
2023-07-11 10:13:04 -0400

    misc changes

--- src/routes/248/+page.svelte
+++ src/routes/248/+page.svelte
@@ -1,15 +1,27 @@
 <!-- https://i.imgur.com/fgk1fpH.png -->
 
-
 <script>
 	import WindowDimensions from '../../lib/components/WindowDimensions.svelte';
 	import ChevronDown from '../../lib/icons/ChevronDown.svelte';
+	import Star from '../../lib/icons/Star.svelte';
 	import Navbar from './Navbar.svelte';
 </script>
 
-
-
 <Navbar />
-<div class="min-h-screen bg-gray-700" />
+<div class="min-h-screen bg-gray-700">
+	<!-- <h1>Recommended for you</h1>
+	<h3>Based on your installs</h3>
+	<div class="flex flex-wrap">
+		<div>
+			<img src="https://placekitten.com/200" alt="" />
+			<div>
+				<span><strong>30 Day Fit Challenge Workout</strong></span>
+				<span>NorthPark.Android</span>
+				<div><Star /><Star /><Star /><Star /><Star /></div>
+				<button class="uppercase">Install App</button>
+			</div>
+		</div>
+	</div> -->
+</div>
 
 <!-- <WindowDimensions /> -->
