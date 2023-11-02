54f3b99110cb5da49163ad138e043f6b28a158de
Parker Bedlan <pbedlan@nepris.com>
2023-11-02 11:19:19 -0400

    resized post-card-2

--- src/routes/post-card-2/+page.svelte
+++ src/routes/post-card-2/+page.svelte
@@ -6,43 +6,43 @@
 
 <div class="grid min-h-screen place-items-center bg-zinc-400 px-4 py-8">
 	<div
-		class="flex h-[30rem] w-72 flex-col gap-3 bg-slate-700 px-7 py-6 text-[11px] text-white shadow-lg shadow-slate-600"
+		class="flex h-auto w-96 flex-col gap-3 bg-slate-700 px-7 py-6 text-white shadow-lg shadow-slate-600"
 	>
 		<img src="https://placekitten.com/800" alt="" class="aspect-[4/3] object-cover object-center" />
 		<div>
-			<h1 class="text-xs font-bold uppercase">3 Myths Everyone Gets Wrong About Yoga</h1>
+			<h1 class="text-lg font-bold uppercase">3 Myths Everyone Gets Wrong About Yoga</h1>
 			<div class="flex items-center justify-between">
 				<div class="text-slate-400">By Adrienne Brien</div>
 				<div class="scale-90 rounded-full bg-slate-800 px-2 py-1">16 April 2017</div>
 			</div>
 		</div>
-		<p class="leading-snug">
+		<p class="pb-6 leading-snug">
 			As an avid yoga lover and teacher, I get really bummed out when people say they don't like
 			yoga or that they don't want to try it. Of course everyone is entitled to their own opinion,
 			but usually they feel this way because of what they've heard or because they had a bad
 			experience.
 		</p>
 		<div
-			class="-mx-7 -mb-6 mt-auto flex h-20 items-center justify-between bg-slate-600 px-6 text-[10px]"
+			class="-mx-7 -mb-6 mt-auto flex h-20 items-center justify-between bg-slate-600 px-6 text-sm"
 		>
 			<div class="flex items-center">
 				<button
-					class="flex items-center gap-[2px] rounded-full px-2 py-1 duration-75 hover:bg-slate-700 active:scale-95"
+					class="flex items-center gap-1 rounded-full px-2 py-1 duration-75 hover:bg-slate-700 active:scale-95"
 				>
-					<Heart isFilled class="w-4 text-red-500" />
+					<Heart isFilled class="w-6 text-red-500" />
 					<div class="mb-[2px]">75 Likes</div>
 				</button>
 				<button
-					class="flex items-center gap-[2px] rounded-full px-2 py-1 duration-75 hover:bg-slate-700 active:scale-95"
+					class="flex items-center gap-1 rounded-full px-2 py-1 duration-75 hover:bg-slate-700 active:scale-95"
 				>
-					<ChatBubbleOvalLeft isFilled class="w-4 text-slate-300" />
+					<ChatBubbleOvalLeft isFilled class="w-6 text-slate-300" />
 					<div class="mb-[2px]">31 Comments</div>
 				</button>
 			</div>
 			<button
-				class="flex items-center gap-[2px] rounded-full px-2 py-1 duration-75 hover:bg-slate-700 active:scale-95"
+				class="flex items-center gap-1 rounded-full px-2 py-1 duration-75 hover:bg-slate-700 active:scale-95"
 			>
-				<Share isFilled class="w-4 text-slate-300" />
+				<Share isFilled class="w-6 text-slate-300" />
 				<div class="mb-[2px] text-slate-300">SHARE</div>
 			</button>
 		</div>
