74d482eac0f2ccd1ae79328a90fe72055d4a446f
Parker Bedlan <pbedlan@nepris.com>
2023-11-10 10:41:55 -0500

    did follow-list

---
+++ src/routes/11-09-23-follow-list/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Profile from './Profile.svelte';
+</script>
+
+<div class="grid min-h-screen place-items-center bg-[#1f2123] text-white">
+	<div
+		class="flex w-96 flex-col rounded-lg bg-[#26282a] px-12 py-8 shadow-[0_10px_100px_-12px_rgb(0_0_0_/_0.6)]"
+	>
+		<Profile
+			name="Michael K. Padilla"
+			job="UX Designer"
+			accentColor="#cf93f7"
+			avatar="https://placekitten.com/200"
+		/>
+		<Profile
+			name="Denice Havens"
+			job="UI Designer"
+			accentColor="#5fefc1"
+			avatar="https://api.dicebear.com/7.x/identicon/svg?seed=Denice"
+		/>
+		<Profile
+			last
+			name="Douglas Davis"
+			job="Web Developer"
+			accentColor="#f5b366"
+			avatar="https://pm1.aminoapps.com/6717/853fa0542c4a453c28da102ab8bcad61c416de9c_hq.jpg"
+		/>
+	</div>
+</div>
---
+++ src/routes/11-09-23-follow-list/Profile.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import ChatBubbleOvalLeft from 'lib/icons/ChatBubbleOvalLeft.svelte';
+	import Plus from 'lib/icons/Plus.svelte';
+
+	export let last = false;
+	export let name: string;
+	export let job: string;
+	export let accentColor: string = '#cf93f7';
+	export let avatar: string;
+</script>
+
+<div class="flex gap-4 border-gray-600 py-4" class:border-b={!last}>
+	<div class="relative z-0 aspect-square w-[100px] flex-shrink-0">
+		<img src={avatar} alt="avatar" class="absolute inset-0 z-0 rounded-r-3xl rounded-bl-3xl" />
+		<div
+			class="absolute -left-1 -top-1 z-10 grid aspect-square w-3 place-items-center rounded-full bg-[#26282a]"
+		>
+			<div class="aspect-square w-1.5 rounded-full" style:background-color={accentColor} />
+		</div>
+	</div>
+	<div class="flex flex-col">
+		<div class="text-lg font-semibold">{name}</div>
+		<div class="text-sm text-gray-500">{job}</div>
+		<div class="mt-2 flex gap-1 text-xs">
+			<button
+				class="flex w-24 items-center justify-end rounded-l-full border border-[#f6705c] py-2 pr-4 text-white duration-75 hover:bg-gray-700 active:scale-95"
+			>
+				<ChatBubbleOvalLeft class="w-4" />
+				<div>Message</div></button
+			>
+			<button
+				class="flex w-24 items-center justify-start rounded-r-full border border-[hsl(8,90%,66%)] bg-[hsl(8,90%,66%)] py-2 pl-4 text-[#26282a] duration-75 hover:bg-[hsl(8,90%,50%)] active:scale-95"
+			>
+				<Plus class="w-4" />
+				<div>Follow</div></button
+			>
+		</div>
+	</div>
+</div>
