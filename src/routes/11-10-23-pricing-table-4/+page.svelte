4abb8a1c1a91a084ee02c44a6658cfc2ba3baa1b
Parker Bedlan <parkerbedlan@gmail.com>
2023-11-26 13:55:44 -0500

    did basic version of pricing-table-4

---
+++ src/routes/11-10-23-pricing-table-4/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Card from './Card.svelte';
+</script>
+
+<!-- https://imgur.com/a/mBsOnJA -->
+<div
+	class="flex min-h-screen items-center justify-center overflow-hidden bg-[#26282a] text-gray-400"
+>
+	<Card
+		monthlyPrice={9}
+		planName="Freelancer"
+		features={['1 GB of space', 'Unlimited traffic', 'Forum access', 'Support at $25/hour']}
+	/>
+	<div class="scale-105">
+		<Card
+			monthlyPrice={99}
+			planName="Business"
+			features={['5 GB of space', 'Unlimited traffic', 'Forum access', 'Support at $5/hour']}
+			blueMode
+		/>
+	</div>
+	<Card
+		monthlyPrice={499}
+		planName="Enterprise"
+		features={['20 GB of space', 'Unlimited traffic', 'Forum access', 'Free support']}
+	/>
+</div>
---
+++ src/routes/11-10-23-pricing-table-4/Card.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let monthlyPrice: number;
+	export let planName: string;
+	export let features: string[];
+	export let blueMode: boolean = false;
+</script>
+
+<div
+	class="w-80 rounded-lg border bg-[#2e3032] shadow-xl"
+	class:text-blue-400={blueMode}
+	class:text-red-400={!blueMode}
+>
+	<div class="flex flex-col items-center justify-center pb-12 pt-12">
+		<div class="flex items-end">
+			<div class="text-5xl font-bold">${monthlyPrice}</div>
+			<div>/mo</div>
+		</div>
+		<div class="uppercase tracking-[.3em]">{planName}</div>
+	</div>
+	<div class="flex flex-col items-center gap-12 rounded-b-lg bg-[#1d1f20] pb-8 pt-12">
+		<div class="flex flex-col gap-2 text-center text-gray-400">
+			{#each features as feature}
+				<div>{feature}</div>
+			{/each}
+		</div>
+		<button
+			class="w-fit rounded-full border px-8 py-2 uppercase duration-75 hover:bg-[#2e3032] active:scale-95"
+			class:border-blue-400={blueMode}
+			class:border-red-400={!blueMode}>Choose Plan</button
+		>
+	</div>
+</div>
